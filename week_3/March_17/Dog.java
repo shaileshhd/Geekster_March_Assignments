59a216da0c384dd4eb8ed4a913609e0b7fee0d3e
Shailesh H D <59162665+shaileshhd@users.noreply.github.com>
2022-03-21 12:03:09 +0530

    Add files via upload

---
+++ week_3/March_17/Dog.java
@@ -0,0 +1,20 @@
+package week3.March_17;
+
+public class Dog {
+	public void speak() {
+		System.out.println("woof!");
+	}
+
+	public static void main(String[] args) {
+		Dog d = new Dog();
+		d.speak();
+		Dog b = new Beagle();
+		b.speak();
+	}
+}
+
+class Beagle extends Dog {
+	public void speak() {
+		System.out.println("arf arf");
+	}
+}
